1db7ac7bc3c26209783cf68dfa21349c8194d6a9
CamillaStrauch <helle.strauch@gmail.com>
2023-09-27 09:06:56 +0200

    Update get_SOR_data.R

--- source/organization/SOR/get_SOR_data.R
+++ source/organization/SOR/get_SOR_data.R
@@ -1,2 +1,6 @@
-p_URL_Sor_complete    <- "https://sor-filer.sundhedsdata.dk/sor2_produktion/V1_00/data/Sor_complete.zip"
+p_URL_edityper_csv <- "https://sor-filer.sundhedsdata.dk/sor_produktion/data/sor/edityper/v_1_2_1/edityper.csv"
+p_URL_sor_csv      <- "https://sor-filer.sundhedsdata.dk/sor_produktion/data/sor/sorcsv/V_1_2_1_19/sor.csv"
+p_URL_sor_zip_xml  <- "https://sor-filer.sundhedsdata.dk/sor_produktion/data/sor/sorxml/v_2_0_0/Sor.zip"
+
+p_URL_Sor_complete_zip    <- "https://sor-filer.sundhedsdata.dk/sor2_produktion/V1_00/data/Sor_complete.zip"
 p_URL_SorEntity_daily <- "https://sor-filer.sundhedsdata.dk/sor2_produktion/V1_00/data/SorEntity_daily.csv"
