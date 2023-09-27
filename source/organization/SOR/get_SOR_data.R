a21c7238721de5547b4270a5c239abfeaf301bee
CamillaStrauch <helle.strauch@gmail.com>
2023-09-27 08:59:14 +0200

    Create get_SOR_data.R

---
+++ source/organization/SOR/get_SOR_data.R
@@ -0,0 +1,2 @@
+p_URL_Sor_complete    <- "https://sor-filer.sundhedsdata.dk/sor2_produktion/V1_00/data/Sor_complete.zip"
+p_URL_SorEntity_daily <- "https://sor-filer.sundhedsdata.dk/sor2_produktion/V1_00/data/SorEntity_daily.csv"
